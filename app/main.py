b0a5d69f204ebda529f59dd224486d706dc3e6e9
eitancohen77 <eitancohen77@gmail.com>
2026-08-04 15:08:49 -0400

    fixed query search

--- app/main.py
+++ app/main.py
@@ -8,10 +8,17 @@ from dotenv import load_dotenv
 from pathlib import Path
 from parsedmockdata import mockdata
 from app.etc.tools import parse_shopping_results
+from fastapi import HTTPException
 
 
 
 app = FastAPI()
+
+SNOW_KEYWORDS = [
+    "snowboard", "board", "binding", "boots", "goggles",
+    "jacket", "pants", "gloves", "helmet", "splitboard", "base layer",
+    "ski"
+]
 
 BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(dotenv_path=Path(__file__).parent / ".env")
@@ -44,6 +51,14 @@ def search_google_shopping(query: str, location: str):
 
 @app.get("/get_products")
 def get_products(q: str, location: str = None):
+
+    query = q.strip().lower()
+
+    if not any(keyword in query for keyword in SNOW_KEYWORDS):
+        raise HTTPException(
+            status_code=400,
+            detail="This search is limited to snowboarding/ski gear."
+        )
 
     # data = search_google_shopping(q, location)
     # parsed_data = parse_shopping_results(data)
